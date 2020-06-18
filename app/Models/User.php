f09cefd01063dae403e83f60c25f9df6b9d9332c
xingwei <xingwei@optaim.com>
2020-06-18 16:14:38 +0800

    update User Model

---
+++ app/Models/User.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * 用户表
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
+     * fillable 变量存储允许自动填充模型字段的数组，可以理解为字段修改 白名单
+     * 去掉我创建的数据表没有的字段
+     * The attributes that are mass assignable.
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'password'
+        //'name', 'email', 'password',
+    ];
+
+    /**
+     * 隐藏部分字段
+     * The attributes that should be hidden for arrays.
+     * @var array
+     */
+    protected $hidden = [
+        'password'
+        //'password', 'remember_token',
+    ];
+
+
+    /**
+     * 将密码进行bcrypt加密
+     * @param $value
+     */
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
+}
