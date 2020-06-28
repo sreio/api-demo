7e343d33cb1164c957b2cfe6f6181a0ca2aa7fc3
xingwei <xingwei@optaim.com>
2020-06-28 10:46:29 +0800

    队列demo

---
+++ app/Http/Controllers/API/PodcastController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Jobs\ProcessPodcast;
+use App\Podcast;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PodcastController extends Controller
+{
+    public function store()
+    {
+        $model = new Podcast();
+        $model->status = 0;
+        $model->save();
+        //触发队列任务
+        //ProcessPodcast::dispatch($model);
+        //一分钟之后执行队列任务
+        ProcessPodcast::dispatch($model)->delay(Carbon::now()->addMinutes(1));
+        echo "添加成功：{$model->id}";
+    }
+}
