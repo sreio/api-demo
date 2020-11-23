b00b199e7bf0e84ddb46f38e381828056e912a89
xingwei <xingwei@optaim.com>
2020-11-23 15:11:23 +0800

    update

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -11,35 +11,41 @@ use Illuminate\Support\Facades\Auth;
 class UserController extends Controller
 {
     //返回用户列表
-    public function index(){
+    public function index()
+    {
         //3个用户为一页
         $users = User::query()->paginate(3);
         return UserResource::collection($users);
     }
     //返回单一用户信息
-    public function show(User $user){
+    public function show(User $user)
+    {
         return $this->success(new UserResource($user));
     }
     //用户注册
-    public function store(UserRequest $request){
+    public function store(UserRequest $request)
+    {
         User::query()->create($request->all());
         return $this->setStatusCode(201)->success('用户注册成功');
     }
     //用户登录
-    public function login(Request $request){
-        $token=Auth::guard('api')->attempt(['name'=>$request->name,'password'=>$request->password]);
-        if($token) {
+    public function login(Request $request)
+    {
+        $token = Auth::guard('api')->attempt(['name' => $request->name, 'password' => $request->password]);
+        if ($token) {
             return $this->setStatusCode(201)->success(['token' => 'bearer ' . $token]);
         }
-        return $this->failed('账号或密码错误',400);
+        return $this->failed('账号或密码错误', 400);
     }
     //用户退出
-    public function logout(){
+    public function logout()
+    {
         Auth::guard('api')->logout();
         return $this->success('退出成功...');
     }
     //返回当前登录用户信息
-    public function info(){
+    public function info()
+    {
         $user = Auth::guard('api')->user();
         return $this->success(new UserResource($user));
     }
